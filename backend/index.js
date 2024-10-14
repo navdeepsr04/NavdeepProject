dc465b3ffe18e948d9c045015ac5ae609b0e5fcc
navdeepsr04 <navdeepsr11@gmail.com>
2024-10-15 00:23:44 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+const port = 4000;
+const express = require("express");
+const app = express();
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+const multer = require("multer");
+const path = require("path");
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
